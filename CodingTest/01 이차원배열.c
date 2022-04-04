df93ed785acbf45c47035d9305ba8894688480d8
chaemin-00515 <w2114@e-mirim.hs.kr>
2022-04-04 20:53:24 +0900

    1차원 배열 오류

--- CodingTest/01 이차원배열.c
+++ CodingTest/01 이차원배열.c
@@ -2,32 +2,10 @@
 
 int main(void)
 {
-	int arr[3][4] = {
-		{1, 2, 3, 4},	//[0][0]:1, [0][1]:2, [0][2]:3, [0][3]:4
-		{5, 6, 7, 8},	//[1][0]:5, [1][1]:6, [1][2]:7, [1][3]:8
-		{9, 10, 11, 12} //[2][0],9, [2][1]:10,[2][2]:11,[2][3]:12
-	};
-
-	//for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++)
-	//{
-	//	printf("%d ", arr[i]);
-	//}
-
-	printf("%d ", arr[0][0]);
-	printf("%d ", arr[0][1]);
-	printf("%d ", arr[0][2]);
-	printf("%d ", arr[0][3]);
-	printf("\n");
-	printf("%d ", arr[1][0]);
-	printf("%d ", arr[1][1]);
-	printf("%d ", arr[1][2]);
-	printf("%d ", arr[1][3]);
-	printf("\n");
-	printf("%d ", arr[2][0]);
-	printf("%d ", arr[2][1]);
-	printf("%d ", arr[2][2]);
-	printf("%d ", arr[2][3]);
-	printf("\n");
-
-
+	int arr[12] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+	// idx는 0부터 시작
+	for (int i = 1; i <= 12; i++)
+	{
+		printf("%d ", arr[i]);
+	}
 }

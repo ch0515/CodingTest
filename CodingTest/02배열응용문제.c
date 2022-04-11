da23d32c0bc2dac76929368a068bf42ab559db85
chaemin-00515 <w2114@e-mirim.hs.kr>
2022-04-11 12:11:29 +0900

    최대값 구하는 것을 함수화

--- CodingTest/02배열응용문제.c
+++ CodingTest/02배열응용문제.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
-int main(void)
-{
-	int arr[10] = { 8,5,1,6,4,9,10,7,11,14 };
+void getMax(int* arr,int length) {
 	int temp = -1;
 	int idx = 99; // 인덱스 값
-	for(int i = 0; i < 10; i++) {
+	for (int i = 0; i < length; i++) {
+
 		if (arr[i] > temp) {
 			temp = arr[i];
 			idx = i;
 		}
 	}
 	printf("값은 :%d, 인덱스 값은 : %d\n", temp, idx);
+}
+int main(void)
+{	
+	int arr1[10] = { 8,5,1,6,4,9,10,7,11,14 };
+	getMax(arr1,10);
+
 	return 0;
 }

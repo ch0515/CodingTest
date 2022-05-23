d54ccd334f9b55d956c32c2b4167773b3e0f8525
chaemin-00515 <w2114@e-mirim.hs.kr>
2022-05-23 11:42:29 +0900

    문자열복사 함수화

--- CodingTest/04문자열 복사하기.c
+++ CodingTest/04문자열 복사하기.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <string.h> //strlen() 호출
-int main(void) {
-	
-	// src에 있는 문자열의 내용을 dest에 복사하세요
+
+//d: 복사할 공간(destination), s: 복사할 문자열(source)
+void my_strcpy(char* d, char* s) {
+	int length = strlen(s);
+	for (int i = 0; i < length; i++) {
+		d[i] = s[i];
+
+	}
+}
+int main(void) {	// src에 있는 문자열의 내용을 dest에 복사하세요
 	char dest[20]; //복사할 공간
 	char src[] = "Hello"; //복사할 데이터
-	int length = strlen(src);
 	//for (int i = 0; src[i] != '\0'; i++)
+
+	//src에 있는 "Hello"를 dest에 복사
+	int length = strlen(src);
+	my_strcpy(dest, src);
 	for (int i = 0; i < length; i++) {
-		dest[i] = src[i];
-		
-	}
-	for (int i = 0; i < 6; i++) {
 		printf("%c", dest[i]);
 	}
 	return 0;

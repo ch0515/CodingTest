69555e382eb9418f4398990b9085ec220d9f5ac5
chaemin <w2114@e-mirim.hs.kr>
2022-11-21 12:03:58 +0900

    03-08완성

---
+++ CodingTest/기출문제/03-08.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+int solution(int programs[][2], int programs_len) {
+    int answer = 0;
+    int used_tv[25] = { 0, };
+
+    //시간대별 방영한 tv의 갯수를 count
+    for (int i = 0; i < programs_len; i++)
+        for (int j = programs[i][0]; j < programs[i][1]; j++)
+            used_tv[j]++;
+
+    //하루에 tv를 2대 이상 트는 총 시간을 return 하도록
+    for (int i = 0; i < 25; i++)
+        if (used_tv[i] >= 2)
+            answer++;
+
+    return answer;
+}
+int main() {
+    int programs[3][2] = { {1, 6}, {3, 5}, {2, 8} };
+    int programs_len = 3;
+    int ret = solution(programs, programs_len);
+
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
+}
